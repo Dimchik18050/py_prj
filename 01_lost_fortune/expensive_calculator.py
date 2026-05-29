8d0c62e77e14df00a0b93338a7b80aeb9a8b679c
Dmytro Khomenko <dimitriy.khomenko@ukr.net>
2026-05-30 00:14:31 +0300

    Додав expensive_calculator.py з математичними операторами

---
+++ 01_lost_fortune/expensive_calculator.py
@@ -0,0 +1,12 @@
+# Expensive Calculator
+# Demonstates built-in aritmetic operators
+
+print("7 + 3 =", 7 + 3)
+print("7 - 3 =", 7 - 3)
+print("7 * 3 =", 7 * 3)
+print("7 ** 3 =", 7 ** 3)
+print("7 / 3 =", 7 / 3)
+print("7 // 3 =", 7 // 3)
+print("7 % 3 =", 7 % 3)
+print("7 + 3 * 5 =", 7 + 3 * 5)
+print("(7 + 3) * 5 =", (7 + 3) * 5)
